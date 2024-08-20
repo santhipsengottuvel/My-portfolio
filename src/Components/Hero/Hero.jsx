48347959c46d398b5561eda050778f55f061585c
santhipsengottuvel <santhipsengottuvel@gmail.com>
2024-08-20 18:10:03 +0530

    changes resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
             </p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'> Connect with me </AnchorLink></div>
-                <div className="hero-resume"><a href="https://drive.google.com/file/d/1ZOcJmRFzHV7J-gI-j9RgvoQdSDQ0xqmC/view?usp=sharing">My resume</a></div>
+                <div className="hero-resume"><a href="https://drive.google.com/file/d/1Rt5nPIJ9DouTBGUN336Hb3EiykUTf0vF/view?usp=drive_link">My resume</a></div>
 
 
 
